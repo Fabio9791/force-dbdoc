149c1c4629f3d4b7829a707c4e678e801f6316bb
fabio <fabiofigueiredo9791@gmail.com>
2018-12-05 15:14:51 +0100

    yes

--- Force/DBDoc/Controller/DocumentorController.php
+++ Force/DBDoc/Controller/DocumentorController.php
@@ -1,10 +1,20 @@
 <?php
 namespace Force\DBDoc\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class DocumentorController
 {
+    private $content;
+    
     public function allAction()
     {
-        return 'Fabio is the best';
+        return new Response('Fabio is the best');
+    }
+    
+    public function setContent(string $content)
+    {
+        $this->content = $content;
+        return $this;
     }
 }
